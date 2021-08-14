d5e1ff8b3ff357748e0f23fdf2eb5cb4bb2ce500
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-14 11:13:40 +0545

    Develop Basic Plot Functions

--- functions.py
+++ functions.py
@@ -3,6 +3,8 @@
 """
 import pandas as pd 
 import numpy as np 
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 """
 function = concat_dataset
@@ -18,5 +20,24 @@ def concat_dataset(company_list : list, path : str ):
     
     return(concat_data)
 
+def figureplot(dataframe, x_axis ,y_aixs, choose_plot):
+    if x_axis == 'date':
+        dataframe[x_axis] = pd.to_datetime(dataframe[x_axis])
+    if choose_plot == "matplot":
+        plt.figure(figsize=(20,12))
+        tech_list = dataframe['Name'].unique()
+        for index, company in enumerate(tech_list,1):
+            plt.subplot(2, 3, index)
+            df=dataframe[dataframe['Name']==company]
+            figure = plt.plot(df[x_axis],df[y_aixs])
+            company_title = plt.title(company)
 
+        return figure, company_title
+
+    elif(choose_plot == 'plotly'):
+          
+        return  (px.line(dataframe, x = x_axis, y = y_aixs, title = ""))
+    
+
+       
 
